8697cb80af3e76bb868f0b12979e28ab6a4fdee0
rajukaji <rajulamarainon@gmail.com>
2025-07-27 21:55:48 +0545

    decorators in python

---
+++ decorators/methoddecorator.py
@@ -0,0 +1,22 @@
+def last_name_decorator(method):
+    def wrapper(self):
+        method(self)
+        print("last name")
+
+    
+    return wrapper
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+
+    @last_name_decorator
+    def first_name(self):
+        print(f"First name: {self.name}")
+
+
+per1 = Person("Ram")
+
+per1.first_name()  # This will call the decorated method
